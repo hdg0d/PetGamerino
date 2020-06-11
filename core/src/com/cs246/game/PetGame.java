96b197ac1c22745005df15b5742e6de2cf41a48e
hdg0d <hugo.dante.castaneda@gmail.com>
2020-06-10 21:05:13 -0500

    THe heckin gamerino

---
+++ core/src/com/cs246/game/PetGame.java
@@ -0,0 +1,60 @@
+package com.cs246.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PetGame extends Game {
+	GameScreen gameScreen;
+	public void create () {
+		gameScreen = new GameScreen();
+		setScreen(gameScreen);
+	}
+
+	@Override
+	public void dispose() {
+		gameScreen.dispose();
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		gameScreen.resize(width, height);
+	}
+}
+
+
+/*  //libGDX generated defaults
+public class PetGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("dog1.png"); //only difference is dog1.png
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
+*/
---
+++ desktop/src/com/cs246/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cs246.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cs246.game.PetGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PetGame(), config);
+	}
+}
