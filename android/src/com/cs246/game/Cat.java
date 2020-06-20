200d01e5e642a1b7c9da828659a0792651425362
Bryson Prince <bryson.prince@yahoo.ca>
2020-06-19 22:15:23 -0600

    Added to pet class and added cat and dog extensions. Added different health condition trackers. Will add more when I hear back from sponsor.

---
+++ android/src/com/cs246/game/Cat.java
@@ -0,0 +1,13 @@
+package com.cs246.game;
+
+public class Cat extends Pet {
+    private Boolean FLVPos;
+
+    public Boolean getFLVPos() {
+        return FLVPos;
+    }
+
+    public void setFLVPos(Boolean FLVPos) {
+        this.FLVPos = FLVPos;
+    }
+}
